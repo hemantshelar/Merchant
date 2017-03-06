00338663931c3be76d849762de3776647e1eda99
hemantshelar <hemant.shelar@gmail.com>
2017-03-06 12:00:30 +1100

    Pagination logic in place.

--- MerchantUI/Scripts/Angular/Controller/HomeCtrl.js
+++ MerchantUI/Scripts/Angular/Controller/HomeCtrl.js
@@ -1,7 +1,24 @@
 ﻿var merchantModule = angular.module("merchantModule");
 
-merchantModule.controller('HomeCtrl', function ($scope, $http) {
+merchantModule.controller('HomeCtrl', function ($scope, $http, MerchantService) {
 
     $scope.Test = "controller in place...";
+    $scope.pagination = undefined;
 
+
+    $scope.myNum = 5;
+
+
+
+    $scope.getMerchants = function () {
+        MerchantService.getMerchants(1, 10).then(function (data) {
+            console.log('data')
+            $scope.pagination = data.pagination;
+        });
+    }
+
+    $scope.nextPage = function (pageNo) {
+        console.log(pageNo);
+        return false;
+    }
 });
---
+++ MerchantUI/Scripts/Angular/Services/MerchantService.js
@@ -0,0 +1,31 @@
+﻿var merchantModule = angular.module("merchantModule");
+
+merchantModule.service('MerchantService', function ($http, $q) {
+
+    var self = this;
+
+    self.getMerchants = function (pageNo, noOfRecordsPerPage) {
+        var defer = $q.defer();
+
+        var url = 'http://api.demo.muulla.com/cms/merchant/all/active';
+        url = url + '/' + noOfRecordsPerPage;
+        url = url + '/' + pageNo;
+       
+
+        //Best way to inject this token is to write interceptor for $http
+        $http.defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NGQxOTY4MGI1MWMxNTI2MGI5NDRmZDUiLCJpc3N1ZV9kYXRlIjoiMjAxNS0wOS0wOVQwNToxMzo1My40NThaIn0.Hk2XypA_KMUnIKdSVYnwq3Rn3QyMNSQ-e80-sZsA9bY';
+
+        $http({
+            url: url,
+            method: 'GET'
+        }).then(function (response) {
+            console.log('got data');
+            defer.resolve(response.data);
+        });
+
+        return defer.promise;
+
+    }
+
+    return self;
+});
